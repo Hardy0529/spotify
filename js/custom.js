e1469efd62c356615bf10f8ff7609140645ea453
Jeff <35370144+Hardy0529@users.noreply.github.com>
2020-07-24 23:10:20 +0800

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+
+    //緩慢滑動
+    $(document).on('click', 'a', function (event) {
+        event.preventDefault();
+        $('html, body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+        }, 500);
+    });
+
+    $(document).ready(function() {
+        $('.owl-carousel').owlCarousel({
+            loop: true,
+            nav: true,
+            items: 1,
+            navText: [],
+            autoplay: true,
+            autoplaySpeed: 1000,
+            smartSpaeed: 500
+        })
+    });
+
+
+})
