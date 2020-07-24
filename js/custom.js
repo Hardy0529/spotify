87a22d537021f7339cc3d8c543c50a63cebb832c
Jeff <35370144+Hardy0529@users.noreply.github.com>
2020-07-24 23:49:50 +0800

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 
     //緩慢滑動
     $(document).on('click', 'a', function (event) {
-        event.preventDefault();
+        // event.preventDefault();
         $('html, body').animate({
         scrollTop: $($.attr(this, 'href')).offset().top
         }, 500);
